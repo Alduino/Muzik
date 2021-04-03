f067d20923b2ce7f03761f9c81b35114748b5d2f
Zach Barham <git@alduino.dev>
2021-04-03 16:42:19 +1000

    Remove ts ignore declarations

--- apps/desktop/src/index.tsx
+++ apps/desktop/src/index.tsx
@@ -6,10 +6,8 @@ process.title = "Muzak";
 
 Renderer.render(<App />);
 
-// @ts-ignore
 if (module.hot) {
     console.log("Using HMR");
 
-    // @ts-ignore
     module.hot.accept("./app", () => Renderer.forceUpdate());
 }
--- apps/desktop/src/startup.ts
+++
@@ -1,5 +0,0 @@
-import {initialise as databaseInit} from "@muzik/database";
-
-export default function start() {
-    databaseInit();
-}
