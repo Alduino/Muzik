e8271a91e69dcb6da96bda88f4e91dce3ab76f30
Zach Barham <git@alduino.dev>
2021-04-03 16:20:36 +1000

    Remove @ at start of logger names

--- libs/logger/src/index.ts
+++ libs/logger/src/index.ts
@@ -3,6 +3,9 @@ import log, {Logger} from "roarr";
 import createSerializeErrorMiddleware from "@roarr/middleware-serialize-error";
 
 export function createLogger(name: string, isApp = false): Logger {
+    // logger already adds the @, we don't want to duplicate it
+    name = name.replace(/^@/, "");
+
     const context = isApp ? {application: name} : {package: name};
 
     return log.child(context).child(createSerializeErrorMiddleware());
