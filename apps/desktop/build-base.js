e1ab8e0fc655d344dfec76c7371ea22b3c8386a1
Zach Barham <git@alduino.dev>
2021-04-05 01:42:44 +1000

    Stop using Babel
    It is significantly slower to build

--- apps/desktop/build-base.js
+++ apps/desktop/build-base.js
@@ -1,4 +1,3 @@
-import babel from "@rollup/plugin-babel";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
@@ -11,12 +10,7 @@ const defaultPlugins = [
     typescript(),
     json(),
     nodeResolve(),
-    commonjs({extensions: [".js", ".ts", ".jsx", ".tsx"]}),
-    babel({
-        babelHelpers: "bundled",
-        extensions: [".js", ".jsx", ".es6", ".es", ".mjs", ".ts", ".tsx"],
-        exclude: "node_modules/*"
-    })
+    commonjs({extensions: [".js", ".ts", ".jsx", ".tsx"]})
 ];
 
 if (process.env.NODE_ENV === "production") {
