ea06a6e368295c1095d0d7e19767a18438d39bc1
Zach Barham <git@alduino.dev>
2021-04-07 16:19:18 +1000

    Speed up album list

--- apps/desktop/src/app/components/AlbumListing.tsx
+++ apps/desktop/src/app/components/AlbumListing.tsx
@@ -1,4 +1,6 @@
+import type {Album as AlbumType} from "@muzik/database";
 import {
+    Box,
     Heading,
     HStack,
     Image,
@@ -6,31 +8,30 @@ import {
     LinkOverlay,
     Skeleton,
     Stack,
-    StackDivider,
-    Text,
-    VStack
+    Text
 } from "@chakra-ui/react";
-import React, {FC} from "react";
-import {Album as AlbumType} from "@muzik/database";
+import React, {CSSProperties, FC, useEffect, useState} from "react";
 import {useAsync} from "react-async-hook";
+import {FixedSizeList} from "react-window";
 import {invoke} from "../../lib/ipc/renderer";
 import {AlbumListResponse, EVENT_ALBUM_LIST} from "../../lib/ipc-constants";
 import useThemeColours from "../hooks/useThemeColours";
 import defaultAlbumArt from "../assets/default-album-art.svg";
-import {ErrorLabel} from "./lib/ErrorLabel";
 import {useSelector} from "react-redux";
 import {RootState} from "../reducers/root";
 import {useAppDispatch} from "../store";
 import {selectAlbum} from "../reducers/albumListingRoute";
+import {ErrorLabel} from "./lib/ErrorLabel";
 
 const fetchAlbums = () => invoke<AlbumListResponse>(EVENT_ALBUM_LIST);
 
 interface AlbumProps {
     album: AlbumType;
     isSelected: boolean;
+    style?: CSSProperties;
 }
 
-const Album: FC<AlbumProps> = ({album, isSelected}) => {
+const Album: FC<AlbumProps> = ({album, isSelected, ...props}) => {
     const colours = useThemeColours();
     const dispatch = useAppDispatch();
 
@@ -43,13 +44,16 @@ const Album: FC<AlbumProps> = ({album, isSelected}) => {
     };
 
     return (
-        <LinkBox width="full">
+        <LinkBox width="full" style={props.style}>
             <HStack
-                width="full"
+                width="calc(100% - 2em)"
                 background={colours.backgroundL2}
                 p={4}
                 borderRadius="sm"
                 shadow={isSelected ? "outline" : "md"}
+                height={128}
+                mt={4}
+                mx={4}
             >
                 <Image src={artPath} width={24} mr={4} borderRadius="sm" />
                 <Stack direction="column">
@@ -67,7 +71,35 @@ const Album: FC<AlbumProps> = ({album, isSelected}) => {
     );
 };
 
+interface AlbumListProps {
+    albums: AlbumType[];
+    selectedAlbum: number;
+    height: number;
+}
+
+const AlbumList: FC<AlbumListProps> = ({albums, selectedAlbum, height}) => (
+    <FixedSizeList
+        itemSize={144}
+        height={height}
+        itemCount={albums.length}
+        itemData={albums.map(item => ({
+            album: item,
+            selected: item.id === selectedAlbum
+        }))}
+        width="100%"
+    >
+        {({data, index, style}) => (
+            <Album
+                album={data[index].album}
+                isSelected={data[index].selected}
+                style={style}
+            />
+        )}
+    </FixedSizeList>
+);
+
 export const AlbumListing: FC = () => {
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
     const albums = useAsync(fetchAlbums, []);
     const selectedAlbum = useSelector<RootState, number>(
         v => v.albumListingRoute.selectedAlbum
@@ -75,44 +107,41 @@ export const AlbumListing: FC = () => {
 
     const colours = useThemeColours();
 
-    return (
-        <VStack
-            mt={24}
-            ml={24}
-            p={4}
-            borderRadius="md"
-            shadow="lg"
-            background={colours.backgroundL1}
-            color={colours.text}
-            width="container.sm"
-            divider={<StackDivider borderColor={colours.backgroundL3} />}
-        >
-            {albums.loading ? (
-                Array.from({length: 4}, (_, i) => (
-                    <Skeleton key={i} width="full">
-                        <Album
-                            album={{
-                                id: -1,
-                                name: "Spooky Scary Skeletons",
-                                artist: {id: -1, name: "The Skeleton Man"},
-                                artistId: -1,
-                                artPath: null
-                            }}
-                            isSelected={false}
-                        />
-                    </Skeleton>
-                ))
-            ) : albums.result ? (
-                albums.result.albums.map(album => (
-                    <Album
-                        key={album.id}
-                        album={album}
-                        isSelected={album.id === selectedAlbum}
+    useEffect(() => {
+        function handler() {
+            setWindowHeight(window.innerHeight);
+        }
+
+        window.addEventListener("resize", handler);
+        return () => window.removeEventListener("resize", handler);
+    });
+
+    if (albums.error) {
+        return <ErrorLabel message={albums.error.message} />;
+    } else {
+        return (
+            <Box
+                mt={24}
+                ml={24}
+                borderRadius="md"
+                shadow="lg"
+                background={colours.backgroundL1}
+                color={colours.text}
+                width="container.sm"
+                overflow="hidden"
+            >
+                {albums.loading ? (
+                    Array.from({length: 4}, (_, i) => (
+                        <Skeleton key={i} width="full" mx={4} mt={4} />
+                    ))
+                ) : (
+                    <AlbumList
+                        albums={albums.result.albums}
+                        selectedAlbum={selectedAlbum}
+                        height={windowHeight - 96 * 2}
                     />
-                ))
-            ) : (
-                <ErrorLabel message={albums.error.message} />
-            )}
-        </VStack>
-    );
+                )}
+            </Box>
+        );
+    }
 };
