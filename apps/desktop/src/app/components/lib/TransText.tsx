cced6864c9db6b1834bd0e77ca16c21d20689af0
Zach Barham <git@alduino.dev>
2021-05-06 11:43:54 +1000

    Remove `as` prop
    Doesn't work, no point including it

--- apps/desktop/src/app/components/lib/TransText.tsx
+++ apps/desktop/src/app/components/lib/TransText.tsx
@@ -1,21 +1,15 @@
-import {As, chakra, Text} from "@chakra-ui/react";
+import {chakra, Text} from "@chakra-ui/react";
 import React from "react";
 import {useTranslation} from "react-i18next";
 
 export interface TransTextProps {
     k: string;
-    as?: As;
     className?: string;
 }
 
 export const TransText = chakra((props: TransTextProps) => {
     const {t} = useTranslation("app");
-
-    return (
-        <Text as={props.as} className={props.className}>
-            {t(props.k)}
-        </Text>
-    );
+    return <Text className={props.className}>{t(props.k)}</Text>;
 });
 
 TransText.displayName = "TransText";
