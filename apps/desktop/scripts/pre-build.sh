eb7b92424c9e805b5a30662f496295808cea9edc
Zach Barham <git@alduino.dev>
2021-05-17 14:13:18 +1000

    Build for multiple Linux distros

--- apps/desktop/scripts/pre-build.sh
+++ apps/desktop/scripts/pre-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+mv build .webpack
+
 # electron-forge outputs assets to the wrong directory
 cd .webpack/renderer || exit
 find . -mindepth 1 -maxdepth 1 ! -name main_window -exec mv -t main_window/ {} +
 cd ../..
+
 mv .webpack build
